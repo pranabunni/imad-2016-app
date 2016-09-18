2d55608b620a998b3d6d7d07f6694aabf25fceff
pranabunni <vadakkamvaikkathur@gmail.com>
2016-09-18 14:35:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send('pranab');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -16,9 +18,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one'),function(req,res){
-    res.send('pranab');
-};
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
